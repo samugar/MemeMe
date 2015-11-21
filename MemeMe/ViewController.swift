f3c78e0e957d781b65877266749faa9257acc3bb
Samuel Garcia <samugar@gmail.com>
2015-11-21 21:25:30 +0100

    Added Camera picker

--- MemeMe/ViewController.swift
+++ MemeMe/ViewController.swift
@@ -12,16 +12,24 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     @IBOutlet weak var imagePickerView: UIImageView!
     
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    
     let pickerController = UIImagePickerController()
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         pickerController.delegate = self
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
     }
 
-    @IBAction func pickAnImage(sender: UIBarButtonItem) {
-        
+    @IBAction func pickAnImageFromAlbum(sender: UIBarButtonItem) {
+        pickerController.sourceType = .PhotoLibrary
+        self.presentViewController(pickerController, animated: true, completion: nil)
+    }
+    
+    @IBAction func pickAnImageFromCamera(sender: UIBarButtonItem) {
+        pickerController.sourceType = .Camera
         self.presentViewController(pickerController, animated: true, completion: nil)
     }
     
